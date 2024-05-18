857f54ae337a19352937e752e40440ec783e7bb1
89645321 <mlove01910@gmail.com>
2024-05-18 13:43:03 +0900

    :sparkles: Implement similarity computing API

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -6,5 +6,6 @@ app_name = 'user'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
-    path('userinfo/', views.userinfo, name='userinfo')
+    path('userinfo/', views.userinfo, name='userinfo'),
+    path('similarity/', views.similarity, name='similarity')
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from .serializers import SignupSerializer
 from .serializers import LoginSerializer
 from .models import User
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+import subprocess
+import os
 
 
 @api_view(['POST'])
@@ -50,4 +54,69 @@ def userinfo(request):
         {"name": user.name, "bank": user.bank, "account": user.account, "balance": user.balance},
         status=status.HTTP_200_OK
     )
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+@api_view(['POST'])
+def similarity(request):
+    import datetime
+    log = open('log.txt', 'a')
+    logtext = 'request: ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logtext += ', ' + get_client_ip(request) + '\n'
+    log.write(logtext)
+    log.close()
+
+    if 'face' not in request.FILES:
+        return Response({"face": "face ciphertext binary file must be provided"}, status=status.HTTP_400_BAD_REQUEST)
+    if 'id_card' not in request.FILES:
+        return Response({"id_card": "id card ciphertext binary file must be provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+    face = request.FILES.get('face')
+    id_card = request.FILES.get('id_card')
+
+    fs = FileSystemStorage(location='/home/ec2-user/Private_Facematch/server/')
+
+    if fs.exists('face.bin'):
+        fs.delete('face.bin')
+    if fs.exists('id_card.bin'):
+        fs.delete('id_card.bin')
+    if os.path.exists('similarity.bin'):
+        os.remove('similarity.bin')
+
+    fs.save('face.bin', face)
+    fs.save('id_card.bin', id_card)
+
+    executable_path = './server/similarity'
+    lib_path = "./server/facematch/"
+
+    env = os.environ.copy()
+    env["LD_LIBRARY_PATH"] = lib_path
+
+    try:
+        result = subprocess.run(executable_path, capture_output=True, text=True, check=True, env=env)
+    except subprocess.CalledProcessError as e:
+        return Response({"error": e.stderr}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        os.remove('./server/face.bin')
+        os.remove('./server/id_card.bin')
+
+        with open("similarity.bin", "rb") as f:
+            similarity_result = f.read()
+        os.remove("similarity.bin")
+
+        response = HttpResponse(similarity_result, content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment; filename=similarity.bin'
+        return response
+
+    except Exception as e:
+        return Response({"error": e.__str__()}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
